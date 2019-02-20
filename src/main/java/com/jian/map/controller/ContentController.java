e740cda883ca487b26c3e80d8297551f2738203e
liujian <449667481@qq.com>
2019-02-20 23:57:38 +0800

    update

--- src/main/java/com/jian/map/controller/ContentController.java
+++ src/main/java/com/jian/map/controller/ContentController.java
@@ -19,6 +19,7 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.FillPatternType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -606,23 +607,25 @@ public class ContentController extends BaseController<Content> {
 	                HSSFRow rowc = sheet.createRow(k+1);
 	                rowc.createCell(0).setCellValue(node.getPid());
 	                rowc.createCell(1).setCellValue(tmp == null ? node.getUser()+"" : tmp.getUsername());
-	                /*if(tmp == null) {
-	                	rowc.createCell(2).setCellValue("");
-	                }else {
-//	                	byte r = (byte)Integer.parseInt(tmp.getColor().substring(1, 3), 16); 
-//	                	byte g = (byte)Integer.parseInt(tmp.getColor().substring(3, 5), 16); 
-//	                	byte b = (byte)Integer.parseInt(tmp.getColor().substring(5, 7), 16); 
+	                if(tmp != null && !Tools.isNullOrEmpty(tmp.getColor())) {
+	                	byte r = (byte)Integer.parseInt(tmp.getColor().substring(1, 3), 16); 
+	                	byte g = (byte)Integer.parseInt(tmp.getColor().substring(3, 5), 16); 
+	                	byte b = (byte)Integer.parseInt(tmp.getColor().substring(5, 7), 16); 
 	                	HSSFCell cellc = rowc.createCell(2);
 //	                	HSSFExtendedColor color = new HSSFExtendedColor(null);
 //	                	color.setRGB(new byte[]{r, g, b, 1});
-		                HSSFPalette customPalette = workbook.getCustomPalette();
-		                customPalette.setColorAtIndex(HSSFColor.ORANGE.index, (byte) 0, (byte) 0, (byte) 0);
+	                	HSSFPalette customPalette = workbook.getCustomPalette();
+	                	customPalette.setColorAtIndex((short)9, r, g, b);
 	                	HSSFCellStyle s = workbook.createCellStyle();
-	                	s.setFillBackgroundColor(HSSFColor.ORANGE.index);
+	                	//s.setFillBackgroundColor((short)9);
+	                	s.setFillForegroundColor((short)9);
+	                	s.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 	                	cellc.setCellStyle(s);
 	                	cellc.setCellValue(tmp.getColor());
-	                }*/
-	                rowc.createCell(2).setCellValue(tmp == null ? "" : tmp.getColor());
+	                }else {
+	                	rowc.createCell(2).setCellValue("");
+	                }
+//	                rowc.createCell(2).setCellValue(tmp == null ? "" : tmp.getColor());
 	                rowc.createCell(3).setCellValue(node.getDate());
 	                rowc.createCell(4).setCellValue( drawTypeNames[node.getType()]);
 	                rowc.createCell(5).setCellValue(node.getPath());
@@ -693,5 +696,17 @@ public class ContentController extends BaseController<Content> {
 		User user = (User)test.getValue();
 		
 		return user;
+	}
+	
+	public static void main(String[] args) {
+		int test1 = Integer.parseInt("DC", 16);
+		int test2 = Integer.parseInt("14", 16);
+		int test3 = Integer.parseInt("14", 16);
+		System.out.println(test1);
+		System.out.println(test2);
+		System.out.println(test3);
+		System.out.println((byte)220);
+		System.out.println((byte)test1);
+		
 	}
 }
